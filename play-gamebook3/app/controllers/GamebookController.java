ad10216d6a35664f48cef1b7c1e313b6ae930757
Igor Luzhanov <luzhanov@gmail.com>
2016-02-26 11:51:44 +0100

    Adding new controller

---
+++ play-gamebook3/app/controllers/GamebookController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class GamebookController extends Controller {
+
+    public static Result index() {
+        return TODO;
+    }
+
+    public static Result createGamebook() {
+        return TODO;
+    }
+
+    public static Result getGamebook(Long id) {
+        return TODO;
+    }
+
+    public static Result updateGamebook(Long id) {
+        return TODO;
+    }
+
+    public static Result deleteGamebook(Long id) {
+        return TODO;
+    }
+
+    public static Result listAllBooks() {
+        String username = session("username");
+        return TODO;
+    }
+
+    public static Result getBook() {
+        return TODO;
+    }
+
+}
