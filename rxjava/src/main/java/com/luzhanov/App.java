9fd189b5d9134a614d02931392e085b26d1a8fb8
Igor Luzhanov <igorl@x-formation.com>
2016-08-17 16:16:06 +0200

    Adding second example

--- rxjava/src/main/java/com/luzhanov/App.java
+++ rxjava/src/main/java/com/luzhanov/App.java
@@ -1,10 +1,17 @@
 package com.luzhanov;
 
-/**
- * Hello world!
- */
+
+import rx.Observable;
+
+import java.util.Collections;
+
 public class App {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
+        Observable<Integer> observable = Observable.from(Collections.singletonList(42));
+
+
     }
 }
--- scala/coursera1/build.sbt
+++ scala/coursera1/build.sbt
@@ -3,4 +3,5 @@ name := "coursera1"
 version := "1.0"
 
 scalaVersion := "2.11.8"
-    
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.6" % "test"
