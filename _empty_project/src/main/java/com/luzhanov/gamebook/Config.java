653af35c17c60e64e88bae873694a82446d245aa
Igor Luzhanov <4629679+luzhanov@users.noreply.github.com>
2025-01-16 12:04:36 +0100

    Empty app update

--- _empty_project/src/main/java/com/luzhanov/gamebook/Config.java
+++
@@ -1,9 +0,0 @@
-package com.luzhanov.gamebook;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("classpath:context.xml")
-public class Config {
-}
